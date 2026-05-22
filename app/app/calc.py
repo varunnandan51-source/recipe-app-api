8bcf267731d8a9effe94db171eb3e74c8c22fdc2
VARUN1992NANDAN <varun.nandan@gmail.com>
2026-05-22 18:48:18 +0530

    Configured docker compose and checks to wait for db git push

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,11 +3,11 @@ Calculator Functions
 """
 
 
-def add(x,y):
-    """Add x and y and return result.""" 
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
 
 
-def subtract(x,y):
-    """Subtract x from y and return result.""" 
+def subtract(x, y):
+    """Subtract x from y and return result."""
     return y - x
